f129c183bd148abe257925c8095e02b22809008f
MinhHuy1507 <ngominhhuy150705@gmail.com>
2026-07-09 13:20:30 +0700

    fix: update config POSTGRES

--- config/postgres.py
+++ config/postgres.py
@@ -3,6 +3,8 @@ import os
 POSTGRES_URL = os.getenv("POSTGRES_URL", "jdbc:postgresql://postgres:5432/airflow")
 POSTGRES_USER = os.getenv("POSTGRES_USER", "airflow")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "airflow")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "airflow")
+POSTGRES_SCHEMA = os.getenv("POSTGRES_SCHEMA", "public")
 POSTGRES_DRIVER = os.getenv("POSTGRES_DRIVER", "org.postgresql.Driver")
 
 # PostgreSQL configuration for Airflow PostgresHook
